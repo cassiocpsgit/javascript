fe4533f77b8a0fc3b69d3bac50af14f299e89f9d
cassiocpsgit <83365369+cassiocpsgit@users.noreply.github.com>
2023-08-14 08:14:31 -0300

    Update script.js

--- aula12ex/ex014/scripts/script.js
+++ aula12ex/ex014/scripts/script.js
@@ -3,8 +3,8 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var sdc = window.document.getElementById('sdc')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 16
+    var hora = data.getHours()
+    //var hora = 18
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'img/dia.png'
