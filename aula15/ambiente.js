d1e0d18ed7a61ff3866006982e2bef30ab9c9901
cassiocpsgit <83365369+cassiocpsgit@users.noreply.github.com>
2023-08-15 12:28:39 -0300

    aula 15

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [5,8,4]
+console.log(`O vetor é ${num}`)
+num[3] = 6
+console.log(`O vetor é ${num}`)
+num.push(7) //adiciona um item no final do vetor
+console.log(`O vetor é ${num}`)
+num.length //conta qtos itens o vetor possui
+num.sort() //coloca em ordem crescente
